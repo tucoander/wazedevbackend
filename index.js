db602ab6df2f17e3f89c7bbe66d31fd4eb3ab144
master <tuco.anderson@gmail.com>
2020-01-14 21:23:42 -0300

    configurando todas as rotas

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello Oministack'});
+app.use(express.json());
+
+// Métodos HTTP: GET(exibir), POST(criar), PUT(alterar) e DELETE(deletar)
+
+// Query Params (GET): request.query (Filtros, ordenação, paginação, ...)
+// Route Params (PUT, DELETE): request.params (Identificar um recurso na alteração ou remoção)
+// Body (POST, PUT): request.body (Dados para criação ou alteração de um registro)
+
+app.post('/users', (request, response) => {
+    console.log(request.body);
+    return response.json({ message: 'Hello Oministack 2'});
 });
 
 app.listen(3333);
