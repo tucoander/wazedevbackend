59831b309dec44dcb6aa4b053b7c3f11772210db
master <tuco.anderson@gmail.com>
2020-01-14 21:06:26 -0300

    iniciando servidor api

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello Oministack'});
+});
+
+app.listen(3333);
