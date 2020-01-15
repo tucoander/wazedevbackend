91708c27a31ae603030139a73d73c76c99684e42
master <tuco.anderson@gmail.com>
2020-01-14 21:38:37 -0300

    instalando mongoose

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-ykpko.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(express.json());
 
@@ -9,6 +15,7 @@ app.use(express.json());
 // Query Params (GET): request.query (Filtros, ordenação, paginação, ...)
 // Route Params (PUT, DELETE): request.params (Identificar um recurso na alteração ou remoção)
 // Body (POST, PUT): request.body (Dados para criação ou alteração de um registro)
+// MongoDB (não-relacional)
 
 app.post('/users', (request, response) => {
     console.log(request.body);
