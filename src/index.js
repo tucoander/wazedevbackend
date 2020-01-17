a829bee5a100b753f04869cbcf8001270b6985fb
master <tuco.anderson@gmail.com>
2020-01-17 16:33:47 -0300

    criando websocket

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-ykpko.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -13,7 +18,7 @@ app.use(cors( ));
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
 
 // Métodos HTTP: GET(exibir), POST(criar), PUT(alterar) e DELETE(deletar)
 
