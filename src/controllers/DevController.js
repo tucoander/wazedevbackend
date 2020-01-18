50a670144029e64d2d9e8a7bb30f48f42969c130
master <tuco.anderson@gmail.com>
2020-01-18 11:01:47 -0300

    finalizando realtime

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 //Funções padrão index (mostrar lista), show (mostrar 1), store, update, destroy
 
@@ -36,6 +37,11 @@ module.exports = {
                 techs: techsArray,
                 location
             });
+            const sendSocketMessageTo = findConnections(
+                { latitude, longitude},
+                techsArray,
+            );
+            sendMessage(sendSocketMessageTo, 'new-dev', dev);
         }
     
         return response.json(dev);
